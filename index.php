87a48d361508c515b87a9c47ccd482cb869b5392
Your Nameecho <csouleymane025@gmail.com>
2021-07-29 18:55:50 +0100

    version1

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
  session_start();
   header("Access-Control-Allow-Origin:*");
   header("Access-Control-Allow-Methods: GET,DELETE,POST, OPTIONS");
-
-   require_once __DIR__.'/route/Route.class.php';
-
    require_once __DIR__.'/route/ClientRoute.class.php';
    require_once __DIR__.'/route/CritereRoute.class.php';
    require_once __DIR__.'/route/BieRoute.class.php';
@@ -19,7 +16,7 @@
            $App=new $class($methode);
 
         }else{
-          $App=new Route($methode);
+          $App=new BieRoute("gets");
         }
         
    }catch(Exception $e){
