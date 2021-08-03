9e1a69d1ebf1bcf8a7a8e67736104e992060d5e4
Your Nameecho <csouleymane025@gmail.com>
2021-08-03 16:23:40 +0100

    version 1

--- view/templete.php
+++ view/templete.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link href="./css/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"  crossorigin="anonymous"></script>
     <title><?= $titre ?></title>
     <style>
